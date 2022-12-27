514db750e3fdd9b18a24550ec4695b54261fa132
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-12-27 12:00:32 -0600

    Class Solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import PokemonList from "./components/PokemonList";
+import PokeCard from "./components/PokeCard";
 
 function App() {
+  const [pokes, setPokes] = useState([]);
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const req = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0"
+      );
+      const res = await req.json();
+      setPokes(res.results);
+    };
+    fetchPokemon();
+  }, []);
+
+  const list = pokes.map((value) => (
+    <PokeCard pokeURL={value.url} key={value.name} />
+  ));
   return (
     <div className="App">
-      <PokemonList />
+      <ol>{list}</ol>
     </div>
   );
 }
